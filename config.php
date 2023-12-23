b7dae4aff6d441c152c11ab7ef8e4dee2b060028
David Yusaku <davidyusaku13@gmail.com>
2023-12-24 01:31:34 +0700

    config

--- config.php
+++ config.php
@@ -1,8 +1,16 @@
 <?php
+
+// LOCAL
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "pawshop";
+
+// PUBLISH
+// $servername = "localhost";
+// $username = "dave3253_david";
+// $password = "D-KILL3DX+";
+// $dbname = "dave3253_kb2";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
